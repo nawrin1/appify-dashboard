c7a674c7d650f28b86152f77fdec590af16f6127
nawrin1 <jannatulnawrin@gmail.com>
2026-02-10 02:43:35 +0600

    project initialised

---
+++ redux/store/slice/DashboardSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+
+interface DashboardState {
+  date: string;
+  user: string;
+  isSidebarOpen: boolean; 
+  isMenuOpen: boolean; 
+}
+
+const initialState: DashboardState = {
+  date: 'Today',
+  user: 'All Users',
+  isSidebarOpen: true,
+  isMenuOpen: false,
+};
+
+export const dashboardSlice = createSlice({
+  name: 'dashboard',
+  initialState,
+  reducers: {
+    setDate: (state, action: PayloadAction<string>) => { state.date = action.payload; },
+    setUser: (state, action: PayloadAction<string>) => { state.user = action.payload; },
+    toggleSidebar: (state) => { state.isSidebarOpen = !state.isSidebarOpen; },
+    toggleMobileMenu: (state) => { state.isMenuOpen = !state.isMenuOpen; },
+    closeMobileMenu: (state) => { state.isMenuOpen = false; },
+  },
+});
+
+export const { setDate, setUser, toggleSidebar, toggleMobileMenu, closeMobileMenu } = dashboardSlice.actions;
+export default dashboardSlice.reducer;
